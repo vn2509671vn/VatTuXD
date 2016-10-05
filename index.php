348b805815289eb516ae222dc913d9ce0b0ebbb5
Tran Giang Minh Thang <vn2509671vn@gmail.com>
2016-10-05 12:10:13 +0000

    Commit file index

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>ThangTGM</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <h1>Welcome to ThangTGM workpace</h1>
+</body>
+</html
